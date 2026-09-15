7f3b20f6e8ae419eb4ce56f500e7843b5c96c18b
Tkatana <tmquintana98@gmail.com>
2026-09-15 03:54:12 +0000

    + initializations, + assigned values, about me modified

--- main.cpp
+++ main.cpp
@@ -1,20 +1,30 @@
-#include <iostream>
-#include <string>
 
-// Lab 3 — Your Name
+
+// Lab 3 — Tyler Quintana
 // CIS 5 Week 03 · Types & variables
-
+ 
+ #include <iostream>
+#include <string>
 int main() {
-  const int CURRENT_YEAR = 2026;
+ 
 
-  // TODO: declare and initialize on the same line
-  // std::string name = ...;
-  // int age = ...;
-  // double height_m = ...;
-  // char initial = ...;
-  // bool student = ...;
+  const int CURRENT_YEAR = 2026;
+  std::string first = "Tyler ";
+  std::string last = "Quintana";
+  int age = 28;
+  double height = 5.10;
+  char initial = 'T';
+  bool student = 1; 
 
   std::cout << "=== About me ===\n";
+  std::cout << "Name: " << first << last << "\n";
+  std::cout << "Age: " << age << "\n";
+  std::cout << "Height: " << height << "\n";
+  std::cout << "Initial: " << initial << "\n";
+  std::cout << "Student: " << student << "\n";
+  std::cout << "Year: " << CURRENT_YEAR << "\n"; 
+ 
+ 
   // TODO: labeled prints from the variable names + CURRENT_YEAR
 
   return 0;
